108f615cdbbde3e9354ba7560bcb6de1af6bee52
Laure Valmier <lvalmier@gmail.com>
2026-06-02 17:24:44 +0200

    Layout shell: sidebar/main structure, header with user dropdown, flash redesign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Initiales pour l'avatar du header (max 2 lettres), à partir du nom sinon de l'email.
+  def user_initials(user)
+    source = user.name.presence || user.email
+    source.split(/[\s@.]+/).reject(&:blank?).first(2).map { |word| word[0] }.join.upcase
+  end
 end
