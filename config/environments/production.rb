ae62db5adef8c4ac4375acd80fc28eacbcf4195d
paul lesueur <lesueur_paul@yahoo.fr>
2026-06-04 15:25:43 +0200

    domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << "www.popwear-pro.fr"
+  config.hosts << "popwear-pro.fr"
+
 end
