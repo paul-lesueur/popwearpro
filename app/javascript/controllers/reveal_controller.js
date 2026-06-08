ba36fb23e94aab2159bf98844f32945f3911472b
Anais Dumont anais.dumont6@gmail.com <anais.dumont6@gmail.com>
2026-06-08 10:37:58 +0200

    design home

---
+++ app/javascript/controllers/reveal_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { delay: Number }
+
+  connect() {
+    this.element.classList.add("reveal--hidden")
+
+    this.observer = new IntersectionObserver(([entry]) => {
+      if (!entry.isIntersecting) return
+      setTimeout(() => {
+        this.element.classList.remove("reveal--hidden")
+        this.element.classList.add("reveal--visible")
+      }, this.delayValue || 0)
+      this.observer.disconnect()
+    }, { threshold: 0.25 })
+
+    this.observer.observe(this.element)
+  }
+
+  disconnect() {
+    this.observer?.disconnect()
+  }
+}
