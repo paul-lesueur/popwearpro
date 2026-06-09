90dd9bc46a316de99f23a6ae4fb0af9ee1340d9d
paul lesueur <lesueur_paul@yahoo.fr>
2026-06-09 14:27:04 +0200

    Fix communications migration

--- db/migrate/20260609085813_remove_kind_from_communications.rb
+++ db/migrate/20260609085813_remove_kind_from_communications.rb
@@ -1,5 +1,5 @@
 class RemoveKindFromCommunications < ActiveRecord::Migration[8.1]
   def change
-    remove_column :communications, :kind, :string
+    remove_column :communications, :kind, :string if column_exists?(:communications, :kind)
   end
 end
